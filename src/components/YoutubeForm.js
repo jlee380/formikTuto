0d21c367950925fc67762b1b9bca953b53285c96
Jonghun Lee <jonghunjhl@gmail.com>
2023-03-06 11:45:27 -0500

    simple formik added being able to track the form state

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,23 +1,52 @@
+import { useFormik } from "formik";
 import React from "react";
 
 function YoutubeForm() {
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			email: "",
+			channel: "",
+		},
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+
+	// console.log("values", formik.values);
 	return (
 		<>
-			<label htmlFor="name">Name</label>
-			<input type="text" id="name" name="name" />
+			<form onSubmit={formik.handleSubmit}>
+				<label htmlFor="name">Name</label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					onChange={formik.handleChange}
+					value={formik.values.name}
+				/>
 
-			<label htmlFor="email">Email</label>
-			<input type="email" id="email" name="email" />
+				<label htmlFor="email">Email</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					onChange={formik.handleChange}
+					value={formik.values.email}
+				/>
 
-			<label htmlFor="channel">Channel</label>
-			<input
-				type="text"
-				id="channel"
-				name="channel"
-				placeholder="YouTube channel name"
-			/>
+				<label htmlFor="channel">Channel</label>
+				<input
+					type="text"
+					id="channel"
+					name="channel"
+					placeholder="YouTube channel name"
+					onChange={formik.handleChange}
+					value={formik.values.channel}
+				/>
 
-			<button>submit</button>
+				<button type="submit">submit</button>
+			</form>
 		</>
 	);
 }
