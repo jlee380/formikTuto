a2a16f20796ab8de2e095a0e198f559dfbbcd0b7
Jonghun Lee <jonghunjhl@gmail.com>
2023-03-07 00:17:24 -0500

    array added being able to handle primary and secondary phone numbers from form submission

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -13,6 +13,7 @@ const initialValues = {
 		facebook: "",
 		twitter: "",
 	},
+	phoneNumbers: ["", ""],
 };
 
 const onSubmit = (values) => {
@@ -94,6 +95,20 @@ function YoutubeForm() {
 				<div className="form-control">
 					<label htmlFor="twitter">twitter profile</label>
 					<Field type="text" id="twitter" name="social.twitter" />
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="primaryPh">Primary phone number</label>
+					<Field type="text" id="primaryPh" name="phoneNumbers[0]" />
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="secondaryPh">Secondary phone number</label>
+					<Field
+						type="text"
+						id="secondaryPh"
+						name="phoneNumbers[1]"
+					/>
 				</div>
 
 				<button type="submit">submit</button>
