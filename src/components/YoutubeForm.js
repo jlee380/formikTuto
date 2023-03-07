2a8e72f5255f4656929c185ce4be83f40003f3f0
Jonghun Lee <jonghunjhl@gmail.com>
2023-03-07 00:12:13 -0500

    nested object added

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,11 +1,18 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import React from "react";
 import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
 	name: "",
 	email: "",
 	channel: "",
+	comments: "",
+	address: "",
+	social: {
+		facebook: "",
+		twitter: "",
+	},
 };
 
 const onSubmit = (values) => {
@@ -28,13 +35,17 @@ function YoutubeForm() {
 				<div className="form-control">
 					<label htmlFor="name">Name</label>
 					<Field type="text" id="name" name="name" />
-					<ErrorMessage name="name" />
+					<ErrorMessage name="name" component={TextError} />
 				</div>
 
 				<div className="form-control">
 					<label htmlFor="email">Email</label>
 					<Field type="email" id="email" name="email" />
-					<ErrorMessage name="email" />
+					<ErrorMessage name="email">
+						{(errorMsg) => {
+							<div className="error">{errorMsg}</div>;
+						}}
+					</ErrorMessage>
 				</div>
 
 				<div className="form-control">
@@ -46,6 +57,43 @@ function YoutubeForm() {
 						placeholder="YouTube channel name"
 					/>
 					<ErrorMessage name="channel" />
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="comments">Comments</label>
+					<Field as="textarea" id="comments" name="comments" />
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="address">address</label>
+					<Field name="address">
+						{(props) => {
+							const { field, form, meta } = props;
+							console.log("render props", props);
+							return (
+								<div>
+									<input
+										type="text"
+										id="address"
+										{...field}
+									/>
+									{meta.touched && meta.error && (
+										<div>{meta.error}</div>
+									)}
+								</div>
+							);
+						}}
+					</Field>
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="facebook">facebook profile</label>
+					<Field type="text" id="facebook" name="social.facebook" />
+				</div>
+
+				<div className="form-control">
+					<label htmlFor="twitter">twitter profile</label>
+					<Field type="text" id="twitter" name="social.twitter" />
 				</div>
 
 				<button type="submit">submit</button>
