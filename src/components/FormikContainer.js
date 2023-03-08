5fc3989fa3b2e8de663a45eaa085391dcbfbdccd
Jonghun Lee <jonghunjhl@gmail.com>
2023-03-08 11:03:23 -0500

    reusable checkbox group

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -10,15 +10,24 @@ function FormikContainer() {
 		{ key: "option2", value: "option2" },
 		{ key: "option3", value: "option3" },
 	];
+	const radioOptions = [
+		{ key: "Select an option", value: "" },
+		{ key: "option1", value: "option1" },
+		{ key: "option2", value: "option2" },
+		{ key: "option3", value: "option3" },
+	];
+
 	const initialValues = {
 		email: "",
 		description: "",
 		selectOption: "",
+		radioOption: "",
 	};
 	const validationSchema = Yup.object({
 		email: Yup.string().required("Required"),
 		description: Yup.string().required("Required"),
 		selectOption: Yup.string().required("Required"),
+		radioOption: Yup.string().required("Required"),
 	});
 	const onSubmit = (values) => console.log("Form data", values);
 
@@ -35,7 +44,6 @@ function FormikContainer() {
 						label="Email"
 						name="email"
 					/>
-					{console.log(formik)}
 					<FormikControl
 						control="textarea"
 						label="Description"
@@ -46,6 +54,12 @@ function FormikContainer() {
 						label="Select a topic"
 						name="selectOption"
 						options={dropdownOptions}
+					/>
+					<FormikControl
+						control="radio"
+						label="Select an option"
+						name="radioOption"
+						options={radioOptions}
 					/>
 					<button type="submit">submit</button>
 				</Form>
